3ce26bf42f3971461e0979cd8eb00cb8657589e4
johnrucker4 <jrucker4@outlook.com>
2023-02-19 17:19:21 -0800

    completed assessment

--- code-challenges.js
+++ code-challenges.js
@@ -6,63 +6,133 @@
 
 // --------------------INSTRUCTOR EXAMPLE: Create a function that takes in two strings and determines which of the two strings has more characters. Use the two sets of test variables provided.
 
-// Pseudo code:
+// Pseudo code:  
+// Create declarations and parameters 
+// Create if statement, using .length and greater symbol that will compare the two strings
+// Create else if statement, using .length and greater symbol that will compare the two strings if the opposite were true
+// Run the function using console.log to verify proper returns
 
-// Set one:
-const fruit1 = "apple"
-const fruit2 = "banana"
-// Expected outcome: "banana"
+// const fruit1 = "apple"
+// const fruit2 = "banana"
 
-// Set two:
-const fruit3 = "cherry"
-const fruit4 = "kiwi"
+// const longerFood = (fruit1, fruit2) => {
+//     if(fruit1.length > fruit2.length)
+//         return `${fruit1}`
+//     else if(fruit2.length > fruit1.length)
+//         return `${fruit2}`
+    
+//     }
+// console.log(longerFood(fruit1 , fruit2))
+
+// // // Expected outcome: "banana"
+
+// const fruit3 = "cherry"
+// const fruit4 = "kiwi"
+
+// const foodLonger = (fruit3, fruit4) => {
+//     if(fruit3.length > fruit4.length)
+//         return `${fruit3}`
+//     else if(fruit4.length > fruit3.length)
+//         return `${fruit4}`
+// }
+// console.log(foodLonger(fruit3, fruit4))
+
 // Expected outcome: "cherry"
 
 // --------------------1) Create a function that takes in a number and determines if the given number is below boiling point, at boiling point, or above boiling point. Boiling point is 212 degrees Fahrenheit. Use the test variables provided below.
 
 // Pseudo code:
+// Create declarations and parameters, where temp is the number as it relates to 212 degrees F
+// Create if, else if statements along with greater/lesser/strictly equal symbols to determine below/at/boiling temps based on variables given
+// Run the function using console.log to verify accuracy of function
 
-const temp1 = 42
+
+// const temp1 = 42
+// const temp2 = 350
+// const temp3 = 212
+
+// const boilingPoint = (temp) => {
+//     if(temp < 212)
+//         return `${temp} is below boiling point.`
+//     else if(temp > 212)
+//         return`${temp} is above boiling point.`
+//     else if(temp === 212)
+//         return `${temp} is at boiling point.`
+// }
+// console.log(boilingPoint(temp1))
+// console.log(boilingPoint(temp2))
+// console.log(boilingPoint(temp3))
+//     42 is below boiling point.
+//     350 is above boiling point.
+//     212 is at boiling point.
 // Expected output: "42 is below boiling point"
-
-const temp2 = 350
 // Expected output: "350 is above boiling point"
-
-const temp3 = 212
 // Expected output: "212 is at boiling point"
 
 // --------------------2) Create the code that will combine the two arrays and return the length using the test variables provided below.
 
 // Pseudo code:
+// Create declarations of Padres World Series runs as arrays
+// Create concatentation of array variables
+// Run using console.log with .length to determine length of combined arrays
 
-const padres1984WorldSeriesRuns = [2, 5, 2, 2, 4]
-const padres1998WorldSeriesRuns = [6, 3, 5, 3]
+// const padres1984WorldSeriesRuns = [2, 5, 2, 2, 4]
+// const padres1998WorldSeriesRuns = [6, 3, 5, 3]
+// const padres1984And1998WSRuns = padres1984WorldSeriesRuns.concat(padres1998WorldSeriesRuns)
+
+// console.log(padres1984And1998WSRuns.length)
+// 9
 // Expected output: 9
 
 // --------------------3) Create the code that will reverse the letters of a string using the test variable provided below.
 
 // Pseudo code:
+// In console.log, use use Mutators to modify output
+// Use .split to turn string into array
+// Use .reverse to reverse array
+// Use .join to combine elements in array
+// Run console.log with .split("").reverse("").join("")
 
-const currentCohort = "Alpha 2023"
+// const currentCohort = "Alpha 2023"
+
+// console.log(currentCohort.split("").reverse("").join(""))
+// 3202 ahplA
 // Expected output: "3202 ahplA"
+// *Feedback request* "I know that I need to restructure the data type in the declared variable with .split.  I need a little more clarification on the 'why'.  Is it because I need to change the string into an array first in order for .reverse to be considered a function?"
 
 // --------------------4) Create the code that will return the last index of the given value from the array using the test variables provided below.
 
 // Pseudo code:
+// In console.log, use Accessor .lastindexof() to return the last index of variables given
+// Run console.log to verify values
 
-const myNumbers = [13, 34, 42, 5, 5, 10, 27, 42, 10]
-const givenValue1 = 42
+// const myNumbers = [13, 34, 42, 5, 5, 10, 27, 42, 10]
+// const givenValue1 = 42
+
+// console.log(myNumbers.lastIndexOf(givenValue1))
+// 7
 // Expected output: 7
 
-const givenValue2 = 10
+// const givenValue2 = 10
+// console.log(myNumbers.lastIndexOf(givenValue2))
+// 8
 // Expected output: 8
 
 // --------------------5) Create the code that will sort all the numbers in an array from largest to smallest. Use the test variables provided below.
 
 // Pseudo code:
+// In console.log, use Mutators to arrange the elements of the array
+// Use .sort to arrange elements in ascending order
+// Use .reverse to arrange elements in descending order
+// Run console.log to verify descending order array elements
 
-const sanDiegoSummerTemperatures = [79, 80, 72, 73, 82, 77, 76]
+// const sanDiegoSummerTemperatures = [79, 80, 72, 73, 82, 77, 76]
+
+// console.log(sanDiegoSummerTemperatures.sort().reverse())
+// 82, 80, 79, 77, 76, 73, 72
 // Expected output: [82, 80, 79, 77, 76, 73, 72]
 
-const sanDiegoWinterTemperatures = [59, 68, 62, 59, 66, 67, 66]
+// const sanDiegoWinterTemperatures = [59, 68, 62, 59, 66, 67, 66]
+// console.log(sanDiegoWinterTemperatures.sort().reverse())
+// 68, 67, 66, 66, 62, 59, 59
 // Expected output: [68, 67, 66, 66, 62, 59, 59]
--- interview-questions.md
+++ interview-questions.md
@@ -6,52 +6,62 @@ First, without external resources. Challenge yourself to answer from memory as i
 
 Then, research the question to expand on your answer. Even if you feel you have answered the question completely, there is always something more to learn. Write your researched answer in your OWN WORDS.
 
-INSTRUCTOR EXAMPLE: What is a conditional statement?
-
-Your answer:
-
-Researched answer:
 
 1. What is git? What is the difference between git and Github?
 
 Your answer:
+Git is the version of code that we use to create/edit.  GitHub is the database where Git is submitted and used to store, modify, and or merge various versions of code. GitHub can allow multiple developers to obtain code to work on that has been submitted.
 
 Researched answer:
+Git is a version control system that we use to manage creation/modification of source code.  GitHub is a cloud-based hosting service that makes it easier to manage Git repositories.  This makes collaboration easier and more organized.
 
 2. Which JavaScript operators will return a Boolean value?
 
 Your answer:
+Any operator that returns an value that is either true or false. For example, my name is John.  If I'm asked, and I reply with 'John', the Boolean return is true.  An answer that isn't 'John', would be false.
 
 Researched answer:
+The Boolean value of an expression is the basis for all Javascript comparisons and conditions that can only have one of two values.
 
 3. What is an index? What is the difference between index and value?
 
 Your answer:
+An index is the location of an element(s) within an array.  A value is what the element represents.  For example, in an array containing two numbers, the numbers themselves are values.  Where those numbers are located in the array, would be considered their index.
 
 Researched answer:
+The index is the position of an element within an array. It starts with 1 and goes on for as long as there are elements in the array.  This is different from value, because the value is what the element represents.
 
 4. Compare and contrast arrays and strings. Please include ways in which they are the same and ways in which they are different.
 
 Your answer:
+An array is a list of information.  A string is text information.  You can have strings in an array.  You can have strings that return information in the form of a list.  However, in order for elements in an array to be returned as a string, you may have to input mutators or accessors into the console.log.
 
 Researched answer:
+An array is a collection of ordered data between brackets.  Each element in an array can be seperated by a comma. Strings are a collection of characters in between quotations.  Strings can be used for elements within an array.  Elements inside of an array can be returned as strings, with the appropriate mutators and/or accessors.
 
 5. Give a brief description of proper pair programming techniques. What are the roles of each person?
 
 Your answer:
+In pair programming, the 'driver', inputs code at the direction of the 'navigator'.
 
 Researched answer:
+Pair programming utilizes the strengths of tackling a workload with multiple people.  One person operates the keyboard while the other communicates data to be inputted. This can help productivity, accountability, focus, accuracy, and comprehension.
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
 1. Higher Order Functions:
+A higher order function allows a function to be executed for all of the elements in an array.
 
 2. Jest:
+Jest is a framework that allows the testing of pieces of code.
 
 3. Objects:
+Objects are key:value pairs inside of curly brackets. They can be used in arrays and each object represent a single element. They can contain functions as well.
 
 4. Method:
+An object that has a function is called a method.  Using the JS keyword 'this' can access the value in an object.
 
 5. Classes:
+Classes are a function that contain data and behavior.  Several pieces of data can be accessed within a class by using: Constructor - creating objects, This - access value in an object, and New - creates new instance of an object.  Classes are always capitalized, use PascalCase, and objects are lower case.  They are also reusable and can store any kind of data.
